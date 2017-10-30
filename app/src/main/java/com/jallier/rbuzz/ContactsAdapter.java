be59f6ddcef73897d56d67dcf332cadf9c0ec251
jallier <justin.hallier+git@gmail.com>
2017-10-30 17:51:10 +1300

    Added handling contacts click

--- app/src/main/java/com/jallier/rbuzz/ContactsAdapter.java
+++ app/src/main/java/com/jallier/rbuzz/ContactsAdapter.java
@@ -1,6 +1,7 @@
 package com.jallier.rbuzz;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import com.amulyakhare.textdrawable.util.ColorGenerator;
 import java.util.List;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+    private static final String TAG = "MyContactsAdapter";
     private List<Contact> contactList;
 
     public ContactsAdapter(List<Contact> ContactsList) {
@@ -23,6 +25,13 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyView
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recycler_contacts, parent, false);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked on the view: " + v.toString());
+            }
+        });
 
         return new MyViewHolder(itemView);
     }
